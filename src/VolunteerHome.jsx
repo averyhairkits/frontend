837ae1fd24bdba4c086f4686550b715c49f16e57
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2025-03-03 22:00:02 -0600

    Basic template

---
+++ src/VolunteerHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VolunteerHome = () => {
+  return (
+    <main>
+      <div className='leftSection'></div>
+      <div className='rightSection'></div>
+    </main>
+  );
+};
+
+export default VolunteerHome;
