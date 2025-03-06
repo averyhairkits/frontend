5a26eabdd85c8b84a3ea12b9baf9f5d77b7559ac
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2025-03-06 17:24:26 -0600

    Added volunteer count button functionality

--- src/pages/home/VolunteerHome.jsx
+++ src/pages/home/VolunteerHome.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 
 import { useCalendarContext } from 'common/contexts/CalendarContext';
 import { useCalendarNav } from 'pages/home/useCalendarNav';
+import { useNumVolunteers } from 'pages/home/useNumVolunteers';
 
 import './VolunteerHome.css';
 
@@ -60,12 +61,48 @@ const Times = () => {
     <div className='timeList'>
       {AllTimes.map((time) => (
         <div className='boldedSymbol' key={time}>
-          {time === '9:00 AM' ? <Icon.Sunrise className='sunIcon' /> : ''}
-          {time === '12:00 PM' ? <Icon.Noon className='sunIcon' /> : ''}
-          {time === '5:00 PM' ? <Icon.Sunset className='sunIcon' /> : ''}
+          {time === '9:00 AM' && <Icon.Sunrise className='sunIcon' />}
+          {time === '12:00 PM' && <Icon.Noon className='sunIcon' />}
+          {time === '5:00 PM' && <Icon.Sunset className='sunIcon' />}
           {time}
         </div>
       ))}
+    </div>
+  );
+};
+
+const NumVolunteers = () => {
+  const {
+    numVolunteers,
+    isOpen,
+    handleNumVolunteersButton,
+    handleOptionsPopUp,
+    optionsPopUpRef,
+  } = useNumVolunteers();
+
+  return (
+    <div className='numVolunteersContainer' ref={optionsPopUpRef}>
+      <button
+        className='numVolunteersButton'
+        onClick={handleNumVolunteersButton}
+      >
+        <Icon.Person className='numVolunteersButtonContent' />
+        {numVolunteers}
+        <Icon.Up className='numVolunteersButtonContent' />
+      </button>
+      {isOpen && (
+        <dialog className='numVolunteersPopUp'>
+          {[1, 2, 3].map((option) => (
+            <option
+              key={option}
+              className={`numVolunteersOption ${numVolunteers === option && 'selected'}`}
+              onClick={() => handleOptionsPopUp(option)}
+            >
+              {option}
+            </option>
+          ))}
+        </dialog>
+      )}
     </div>
   );
 };
@@ -119,13 +156,7 @@ const VolunteerHome = () => {
         <CalendarNav currentDate={currentDate} weekdates={weekdates} />
         <div className='rightMainSection'>
           <div className='timeContainer'>
-            <div className='numVolunteersContainer'>
-              <button className='numVolunteersButton'>
-                <Icon.Person className='numVolunteersContent' />
-                3
-                <Icon.Up className='numVolunteersContent' />
-              </button>
-            </div>
+            <NumVolunteers />
             <Times />
           </div>
           <div className='gridContainer'>
