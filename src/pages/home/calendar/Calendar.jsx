8fc48b69c2cd13f2647ba665d9fffbb47fb9eab5
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2025-03-06 22:21:58 -0600

    Separated CalendarNav and Calendar files

--- src/pages/home/calendar/Calendar.jsx
+++ src/pages/home/calendar/Calendar.jsx
@@ -6,61 +6,8 @@ import PropTypes from 'prop-types';
 import { useCalendarContext } from 'common/contexts/CalendarContext';
 import 'pages/home/Home.css';
 import 'pages/home/VolunteerHome.css';
-import { useCalendarNav } from 'pages/home/calendar/useCalendarNav';
+import { CalendarNav } from 'pages/home/calendar/CalendarNav';
 import { useNumVolunteers } from 'pages/home/calendar/useNumVolunteers';
-
-const CalendarNav = ({
-  todaysDate,
-  thisWeeksStart,
-  currentDate,
-  weekdates,
-}) => {
-  const { handlePrevWeek, handleNextWeek } = useCalendarNav();
-  const hasMonthOverlap = weekdates[0].getMonth() !== weekdates[6].getMonth();
-  const atTodaysWeek =
-    thisWeeksStart.toLocaleDateString() !== weekdates[0].toLocaleDateString();
-  const isFourWeeksAhead =
-    currentDate.getTime() - todaysDate.getTime() > 14 * 24 * 60 * 60 * 1000;
-
-  return (
-    <nav className='calendarNav'>
-      {atTodaysWeek ? (
-        <button onClick={handlePrevWeek} className='calendarNavButton'>
-          <Icon.Back className='calendarNavIcon' />
-        </button>
-      ) : (
-        <button className='calendarNavButton' />
-      )}
-      <h6>
-        {currentDate.toLocaleDateString(undefined, {
-          month: 'long',
-          day: 'numeric',
-        })}
-        {' - '}
-        {hasMonthOverlap
-          ? weekdates[6].toLocaleDateString(undefined, {
-              month: 'long',
-              day: 'numeric',
-            })
-          : weekdates[6].toLocaleDateString(undefined, { day: 'numeric' })}
-      </h6>
-      {isFourWeeksAhead ? (
-        <button className='calendarNavButton' />
-      ) : (
-        <button onClick={handleNextWeek} className='calendarNavButton'>
-          <Icon.Next className='calendarNavIcon' />
-        </button>
-      )}
-    </nav>
-  );
-};
-
-CalendarNav.propTypes = {
-  thisWeeksStart: PropTypes.instanceOf(Date).isRequired,
-  todaysDate: PropTypes.instanceOf(Date).isRequired,
-  currentDate: PropTypes.instanceOf(Date).isRequired,
-  weekdates: PropTypes.arrayOf(PropTypes.instanceOf(Date)).isRequired,
-};
 
 const Times = () => {
   const AllTimes = [
@@ -163,16 +110,10 @@ const CalendarGrid = () => {
 };
 
 export const Calendar = () => {
-  const { todaysDate, thisWeeksStart, currentDate, weekdates } =
-    useCalendarContext();
+  const { weekdates } = useCalendarContext();
   return (
     <div className='calendar'>
-      <CalendarNav
-        todaysDate={todaysDate}
-        thisWeeksStart={thisWeeksStart}
-        currentDate={currentDate}
-        weekdates={weekdates}
-      />
+      <CalendarNav />
       <div className='rightMainSection'>
         <div className='timeContainer'>
           <NumVolunteers />
---
+++ src/pages/home/calendar/CalendarNav.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Icon } from 'assets/icons/icons';
+import PropTypes from 'prop-types';
+
+import { useCalendarContext } from 'common/contexts/CalendarContext';
+import 'pages/home/Home.css';
+import 'pages/home/VolunteerHome.css';
+import { useCalendarNav } from 'pages/home/calendar/useCalendarNav';
+
+export const CalendarNav = () => {
+  const { handlePrevWeek, handleNextWeek } = useCalendarNav();
+  const { todaysDate, thisWeeksStart, currentDate, weekdates } =
+    useCalendarContext();
+  const hasMonthOverlap = weekdates[0].getMonth() !== weekdates[6].getMonth();
+  const atTodaysWeek =
+    thisWeeksStart.toLocaleDateString() !== weekdates[0].toLocaleDateString();
+  const isFourWeeksAhead =
+    currentDate.getTime() - todaysDate.getTime() > 14 * 24 * 60 * 60 * 1000;
+
+  return (
+    <nav className='calendarNav'>
+      {atTodaysWeek ? (
+        <button onClick={handlePrevWeek} className='calendarNavButton'>
+          <Icon.Back className='calendarNavIcon' />
+        </button>
+      ) : (
+        <button className='calendarNavButton' />
+      )}
+      <h6>
+        {currentDate.toLocaleDateString(undefined, {
+          month: 'long',
+          day: 'numeric',
+        })}
+        {' - '}
+        {hasMonthOverlap
+          ? weekdates[6].toLocaleDateString(undefined, {
+              month: 'long',
+              day: 'numeric',
+            })
+          : weekdates[6].toLocaleDateString(undefined, { day: 'numeric' })}
+      </h6>
+      {isFourWeeksAhead ? (
+        <button className='calendarNavButton' />
+      ) : (
+        <button onClick={handleNextWeek} className='calendarNavButton'>
+          <Icon.Next className='calendarNavIcon' />
+        </button>
+      )}
+    </nav>
+  );
+};
+
+CalendarNav.propTypes = {
+  thisWeeksStart: PropTypes.instanceOf(Date).isRequired,
+  todaysDate: PropTypes.instanceOf(Date).isRequired,
+  currentDate: PropTypes.instanceOf(Date).isRequired,
+  weekdates: PropTypes.arrayOf(PropTypes.instanceOf(Date)).isRequired,
+};
